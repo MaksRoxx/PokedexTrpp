1460a01b225de67f96c8b6b3ff485a408b717558
MaksRoxx <maksimusroxx@gmail.com>
2024-05-23 00:37:57 +0300

    red

--- app/src/main/java/com/example/composepokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/composepokedex/pokemonlist/PokemonListScreen.kt
@@ -183,7 +183,7 @@ fun PokedexEntry(
 
     Box(
         contentAlignment = Center,
-        modifier = Modifier
+        modifier = modifier
             .shadow(5.dp, RoundedCornerShape(10.dp))
             .clip(RoundedCornerShape(10.dp))
             .aspectRatio(1f)
